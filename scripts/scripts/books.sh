617d9569c0cada2efa06e234692d61c492097ea3
MuhammadSwa <muhammadatefsw@protonmail.com>
2026-05-31 22:00:00 +0300

    commit

--- scripts/scripts/books.sh
+++ scripts/scripts/books.sh
@@ -9,7 +9,12 @@
 # chosen_book=$(printf "%s\n" "${books[@]}" | rofi -dmenu -p "books")
 
 # chosen_book=$(find ~/Books/ /media/Maind/كتب2/ /media/Maind/كتب/ /media/Maind/Dental_books/ -type f ! -path "$HOME/Books/site/*" | rofi -dmenu -p "books" -keep-right -i)
-chosen_book=$(find ~/Books/ /media/Maind/كتب2/ /media/Maind/كتب/ /media/Maind/Dental_books/ -type f ! -path "$HOME/Books/site/*" | ~/Mzahrawi/productivity/badi/zig-out/bin/Badi)
+# chosen_book=$(find ~/Books/ /media/Maind/كتب2/ /media/Maind/كتب/ /media/Maind/Dental_books/ -type f ! -path "$HOME/Books/site/*" | ~/Mzahrawi/productivity/badi/zig-out/bin/badi)
+tmpfile=$(mktemp)
+find ~/Books/ /media/Maind/كتب2/ /media/Maind/كتب/ /media/Maind/Dental_books/ -type f ! -path "$HOME/Books/site/*" >"$tmpfile"
+chosen_book=$(sed 's|.*/\([^/]*\)/\([^/]*\)$|\1/\2|' "$tmpfile" | ~/Mzahrawi/productivity/badi/zig-out/bin/badi)
+chosen_book=$(grep -F "/$chosen_book" "$tmpfile" | head -1)
+rm -f "$tmpfile"
 
 [[ $? -eq 1 ]] && exit 1
 # echo $chosen_book
