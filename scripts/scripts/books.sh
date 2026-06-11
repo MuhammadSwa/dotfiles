ed451610afd7f724a7cdc0b66b43faff4478ef16
MuhammadSwa <muhammadatefsw@protonmail.com>
2026-06-12 01:00:00 +0300

    commit

--- scripts/scripts/books.sh
+++ scripts/scripts/books.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# chosen_book=$(find ~/Books/ /media/Maind/كتب2/ /media/Maind/كتب/ /media/Maind/Dental_books/ -type f ! -path "$HOME/Books/site/*" | rofi -dmenu -p "books" -keep-right -i)
 tmpfile=$(mktemp)
 find ~/Books/ /media/Maind/كتب2/ /media/Maind/كتب/ /media/Maind/Dental_books/ -type f ! -path "$HOME/Books/site/*" >"$tmpfile"
 chosen_book=$(sed 's|.*/\([^/]*\)/\([^/]*\)$|\1/\2|' "$tmpfile" | ~/Mzahrawi/productivity/badi/zig-out/bin/badi)
@@ -8,8 +7,4 @@ chosen_book=$(grep -F "/$chosen_book" "$tmpfile" | head -1)
 rm -f "$tmpfile"
 
 [[ $? -eq 1 ]] && exit 1
-# echo $chosen_book
-# xpdf -z 200 -open "$chosen_book"
 sioyek "$chosen_book"
-# okular "$chosen_book"
-# zathura "$chosen_book"
