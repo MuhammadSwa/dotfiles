70c118f3cd8fe3b00a9d96db3b94db1d3e7d9c8b
MuhammadSwa <muhammadatefsw@protonmail.com>
2026-06-16 21:00:00 +0300

    commit

--- scripts/scripts/sselp.sh
+++ scripts/scripts/sselp.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-path="$HOME/MyVault/Areas/Dentistry/Dental caries.md"
+# path="$HOME/MyVault/Areas/Dentistry/Dental caries.md"
+path="$HOME/Mzahrawi/ongoing/qtzig/notes/Essential-modules.md"
 # echo "- $(sselp)" >> "/media/Maind/MyVault/highlights.md"
-echo "- $(wl-paste -p)" >> "/media/Maind/MyVault/highlights.md"
+# echo "- $(wl-paste -p)" >> "/media/Maind/MyVault/highlights.md"
+echo "- $(wl-paste -p)" >> "$path"
 # xsel >> ~/MyVault/highlights.md
 # sselp >> ~/MyVault/highlights.md
 # printf "\n" >> ~/MyVault/highlights.md
