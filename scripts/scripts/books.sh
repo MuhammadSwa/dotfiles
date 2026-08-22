855240eca2dc5d60b840e2150ce33dc75637e872
MuhammadSwa <muhammadatefsw@protonmail.com>
2026-08-23 01:00:00 +0300

    commit

--- scripts/scripts/books.sh
+++ scripts/scripts/books.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
+#
+# tmpfile=$(mktemp)
+# find ~/Books/ /media/Maind/كتب2/ /media/Maind/كتب/ /media/Maind/Dental_books/ -type f ! -path "$HOME/Books/site/*" >"$tmpfile"
+# # chosen_book=$(sed 's|.*/\([^/]*\)/\([^/]*\)$|\1/\2|' "$tmpfile" | ~/Mzahrawi/productivity/badi/zig-out/bin/badi)
+# chosen_book=$(sed 's|.*/\([^/]*\)/\([^/]*\)$|\1/\2|' "$tmpfile" | ~/Mzahrawi/ongoing/badi/zig-out/bin/gtk-zig)
+# chosen_book=$(grep -F "/$chosen_book" "$tmpfile" | head -1)
+# rm -f "$tmpfile"
+#
+# [[ $? -eq 1 ]] && exit 1
+# sioyek "$chosen_book"
 tmpfile=$(mktemp)
-find ~/Books/ /media/Maind/كتب2/ /media/Maind/كتب/ /media/Maind/Dental_books/ -type f ! -path "$HOME/Books/site/*" >"$tmpfile"
-# chosen_book=$(sed 's|.*/\([^/]*\)/\([^/]*\)$|\1/\2|' "$tmpfile" | ~/Mzahrawi/productivity/badi/zig-out/bin/badi)
-chosen_book=$(sed 's|.*/\([^/]*\)/\([^/]*\)$|\1/\2|' "$tmpfile" | ~/Mzahrawi/ongoing/badi/zig-out/bin/gtk-zig)
-chosen_book=$(grep -F "/$chosen_book" "$tmpfile" | head -1)
-rm -f "$tmpfile"
+trap 'rm -f "$tmpfile"' EXIT
 
-[[ $? -eq 1 ]] && exit 1
-sioyek "$chosen_book"
+chosen=$(
+    fd --type f --hidden --no-ignore -E 'site' \
+        . "$HOME/Books" /media/Maind/كتب2 /media/Maind/كتب /media/Maind/Dental_books \
+    | tee "$tmpfile" \
+    | sed 's|.*/\([^/]*\)/\([^/]*\)$|\1/\2|' \
+    | ~/Mzahrawi/ongoing/badi/zig-out/bin/gtk-zig
+) || exit 1
+
+[[ -n $chosen ]] || exit 1
+
+path=$(grep -F "/$chosen" "$tmpfile" | head -n1)
+[[ -n $path ]] && exec sioyek "$path"
